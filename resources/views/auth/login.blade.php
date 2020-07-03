65794d9678f39996d8a02b8c44c1ad4914f57910
JMorales <josephzx1@hotmail.com>
2020-07-03 00:45:00 -0600

    Se agrega css a login.balde.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,18 +2,6 @@
 
 @section('content')
 
-<<<<<<< HEAD
-<section class="main">		
-    <section class="login">
-        <form action="#" class="login__form">
-            <div class="login__exit--button"> <a href="{{ url('/') }}" class="login__exit--link"><i class="fas fa-times-circle exit-button-i"></i> </a> </div>
-            <h1 class="login__form--title">Cuenta Monso</h1>
-            <input class="login__form--text" type="email" name="txtEmail" placeholder="Correo electrónico">
-            <input  class="login__form--text" type="password" name="txtPassword" placeholder="Contraseña">
-            <input class="login__form--submit" type="submit" value="Iniciar Sesión">
-        </form>
-    </section>
-=======
 <section class="main">
     
 	<section class="login">
@@ -35,7 +23,6 @@
 			<input class="login__form--submit" type="submit" value="Iniciar Sesión">
 		</form>
 	</section>
->>>>>>> rm_Negro
 </section>
 
 @endsection
