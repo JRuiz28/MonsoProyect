61f11d2baac9a839cb75a5e09872d8b752181deb
Josué Ruiz <jrc2899@gmail.com>
2020-07-04 00:14:40 -0600

    Ajuste para iniciar en la pagina default

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,8 +15,7 @@
 
 	<!--JS-->
 	<script text="text/javaScript" src="JS/index.js"></script>     
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/fontawesome.min.css"
-	></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/fontawesome.min.css"></script>
 	
 	
     <!--Others-->
@@ -26,24 +25,6 @@
 </head>
 <body>
     <div id="app">
-
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        
-                    </ul>
-                </div>
-            </div>
-        </nav>
 
         <main class="py-4">
             @yield('content')
