cccd74292e48d6a909f1074f5cf744a4e91b0d46
JMorales <josephzx1@hotmail.com>
2020-07-04 23:02:09 -0600

    Se remueve la carpeta Errors

--- resources/views/Errors/403.blade.php
+++
@@ -1,4 +0,0 @@
-@extends('errors::layout')
-
-@section('title', '403 - Acceso no autorizado')
-@section('message', 'Acceso no autorizado')   
--- resources/views/Errors/404.blade.php
+++
@@ -1,4 +0,0 @@
-@extends('errors::layout')
-
-@section('title', '404 - Página no encontrada')
-@section('message', 'Putos todos, no hay ni verga') 
