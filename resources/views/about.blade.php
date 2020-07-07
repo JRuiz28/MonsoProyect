5e73954760107411d8f0101572e8f1f11e493873
Josue <josue7114@gmail.com>
2020-07-07 16:48:30 -0600

    cambio css de about us a scss

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -6,13 +6,13 @@
 	<!--End - Navbars-->
 
 	<!--start body-->
-	<article class="article">
-		<div class="container">
-			<img class="img" src="static/img/Logo_MONSO.png" alt="Monso Cosméticos Costa Rica">
+	<article class="article-about">
+		<div class="container-about">
+			<img class="container__img-about" src="static/img/Logo_MONSO.png" alt="Monso Cosméticos Costa Rica">
 		</div>
-		<h1 class="article__title">Nuestra empresa</h1>
+		<h1 class="article-about__title">Nuestra empresa</h1>
 
-		<p class="article__paragraph">
+		<p class="article__paragraph-about">
 			Somos un emprendimiento familiar creado en enero del 2020,  con la intención de abarcar las necesidades de nuestro clientes enfocándonos en el área de cuidado personal.
 			Manejamos una amplia línea de productos de cosmética natural tales como jabónes sólidos faciales - corporales, jabónes líquidos, cremas, exfoliantes, mascarillas, shampoo, acondionador, bálsamos entre otros.
 			Cabe destacar que todos nuestros productos están elaborados con materia prima de la más alta calidad, teniendo un enfoque 100% natural, utilizando gran variedad de hierbas frescas, infusiones, especies y aceites esenciales.
