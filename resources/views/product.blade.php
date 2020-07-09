44fba9f56343be735fa438afd1c0e7f1a4fdc461
JMorales <josephzx1@hotmail.com>
2020-07-09 15:00:01 -0600

    Se agrega css de  parte movil de producto individual

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -6,40 +6,48 @@
 	<!--End - Navbars-->
 	<!--start body-->
 	<section class="main">
-		<section class="main_container">
-			<h1 class="product_title">Jabón Manzanilla y miel</h1>
+		<section class="main__container">
+			<h1 class="product__title">Jabón Manzanilla y miel</h1>
 			<article class="product">
 				<figure class="product__imgContainer">
 					<!-- <div class="product__img"></div> -->
 					<img class="product__img" src="{{asset('static/img/avena.jpeg')}}" alt="Imagen de muestra del producto"/>
 				</figure>
 				<article class="product__detail">
-					<label class="product__text">Precio:</label class="text-product"><label>₡5.000</label>
+					<label class="product__text"><strong>Precio:</strong></label class="text-product"><label>₡5.000</label>
 					<label class="product__text">Cantidad:</label class="text-product"><label>100</label>
 					<label class="product__text">Estado del producto:</label class="text-product"><label>Disponible</label>
 				</article>
 			</article>
-			<div class="container-description">
-				<h2 class="container-desc__subtitle-description">Descripción</h2>
-				<p class="container-desc__paragraph">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officia, temporibus quod. 
+			<section class="description">
+				<h2 class="description__title">Descripción</h2>
+				<p class="description__detail">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officia, temporibus quod. 
 					Amet, quo cum doloribus earum harum excepturi, a ducimus autem alias quisquam explicabo, 
-					quaerat odio? Sunt impedit praesentium voluptas!
+					quaerat odio? Sunt impedit praesentium voluptas! Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, repudiandae asperiores. Incidunt asperiores, consequatur sunt cupiditate quis tenetur laboriosam dicta magnam beatae quam dolore pariatur quaerat nostrum? Nam, magni debitis!
 				</p>
-			</div>
-			<section class="related-products">
-				<h2 class="product_subtitle">Productos relacionados<h2>
-				<!-- <div class="cointainer-related cointainer-related--left">
-					<img class="container-related__img" src={{asset('static/img/avena.jpeg')}} alt="Productos relacionados"/>
-					<button class="container-related__button">Ver más</button>
-				</div>
-				<div class="cointainer-related cointainer-related--center">
-					<img class="container-related__img" src={{asset('static/img/avena.jpeg')}} alt="Productos relacionados"/>
-					<button class="container-related__button">Ver más</button>
-				</div>
-				<div class="cointainer-related cointainer-related--right">
-					<img class="container-related__img" src={{asset('static/img/avena.jpeg')}} alt="Productos relacionados"/>
-					<button class="container-related__button">Ver más</button>
-				</div> -->
+			</section>
+			<section class="related">
+				<h2 class="related__title">Productos relacionados<h2>
+				<section class="related__container">
+					<section class="related__product">
+						<figure class="related__imgContainer">
+							<img class="related__img" src={{asset('static/img/avena.jpeg')}} alt="Productos relacionados"/>
+							<button class="related__btn">Ver más</button>
+						</figure>
+					</section>
+					<section class="related__product">
+						<figure class="related__imgContainer">
+							<img class="related__img" src={{asset('static/img/avena.jpeg')}} alt="Productos relacionados"/>
+							<button class="related__btn">Ver más</button>
+						</figure>
+					</section>
+					<section class="related__product">
+						<figure class="related__imgContainer">
+							<img class="related__img" src={{asset('static/img/avena.jpeg')}} alt="Productos relacionados"/>
+							<button class="related__btn">Ver más</button>
+						</figure>
+					</section>
+				</section> 
 			</section>
 		</section>
 	</section>
