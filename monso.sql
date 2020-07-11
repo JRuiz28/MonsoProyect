8e81dc234aed864159aa957f313b6a8d97644abf
Josué Ruiz <jrc2899@gmail.com>
2020-07-11 17:28:25 -0600

    BD con los productos

--- monso.sql
+++ monso.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 02-07-2020 a las 08:24:51
+-- Tiempo de generación: 12-07-2020 a las 01:27:36
 -- Versión del servidor: 10.4.11-MariaDB
 -- Versión de PHP: 7.4.6
 
@@ -24,44 +24,123 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `failed_jobs`
+--
+
+CREATE TABLE `failed_jobs` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `connection` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `queue` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `payload` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `exception` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `failed_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `ingredients`
 --
 
 CREATE TABLE `ingredients` (
-  `id` int(255) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `created_at` datetime NOT NULL,
-  `updated_at` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `ingredients`
+--
+
+INSERT INTO `ingredients` (`id`, `name`, `created_at`, `updated_at`) VALUES
+(1, 'Avena', NULL, NULL),
+(2, 'Arcilla', NULL, NULL),
+(3, 'Aceite de oliva', NULL, NULL),
+(4, 'Árbol de té', NULL, NULL),
+(5, 'Banana', NULL, NULL),
+(6, 'Cacao', NULL, NULL),
+(7, 'Canela', NULL, NULL),
+(8, 'Coco', NULL, NULL),
+(9, 'Macadamia', NULL, NULL),
+(10, 'Menta', NULL, NULL);
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `product`
+-- Estructura de tabla para la tabla `migrations`
 --
 
-CREATE TABLE `product` (
-  `id` int(255) NOT NULL,
-  `name` int(255) NOT NULL,
-  `description` text NOT NULL,
-  `stock` int(255) NOT NULL,
+CREATE TABLE `migrations` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+(7, '2014_10_12_000000_create_users_table', 1),
+(8, '2014_10_12_100000_create_password_resets_table', 1),
+(9, '2019_08_19_000000_create_failed_jobs_table', 1),
+(10, '2020_07_03_071229_create_ingredients_table', 1),
+(11, '2020_07_03_071306_create_products_table', 1),
+(12, '2020_07_03_071326_create_product_ingredients_table', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `password_resets`
+--
+
+CREATE TABLE `password_resets` (
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `products`
+--
+
+CREATE TABLE `products` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `description` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `stock` int(11) NOT NULL,
   `price` double NOT NULL,
-  `image_path` varchar(255) NOT NULL,
-  `created_at` datetime NOT NULL,
-  `updated_at` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `image_path` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `products`
+--
+
+INSERT INTO `products` (`id`, `name`, `description`, `stock`, `price`, `image_path`, `created_at`, `updated_at`) VALUES
+(24, 'Arnica y manzanilla', 'Potente hidratante - Antiinflamatorio - Regenerador - Antiarrugas - Suaviza la Dermis y múltiples beneficios más!!!', 50, 9995, 'public/udwvh4z1kZvbJLywpjlWnzPp3VVNVb9SdWDY4tm4.jpeg', '2020-07-12 03:22:50', '2020-07-12 03:34:00'),
+(25, 'Avena', 'Potente hidratante - Antiinflamatorio - Regenerador - Antiarrugas - Suaviza la Dermis y múltiples beneficios más!!!', 9, 9495, 'public/SGBEVZ8EDeKHQjXU5c9cvdPeCeIm4dbp3rFPwG45.jpeg', '2020-07-12 03:23:16', '2020-07-12 03:23:16'),
+(26, 'Aloe Vera', 'Potente hidratante - Antiinflamatorio - Regenerador - Antiarrugas - Suaviza la Dermis y múltiples beneficios más!!!', 12, 4995, 'public/fr8x9GZeYCgaN3Tox1kHKBGrp4jIDWe3nGCGrRwA.jpeg', '2020-07-12 03:28:52', '2020-07-12 03:28:52'),
+(27, 'Leche de cabra', 'Potente hidratante - Antiinflamatorio - Regenerador - Antiarrugas - Suaviza la Dermis y múltiples beneficios más!!!', 10, 6995, 'public/2M08o1sl0SQYWMj6OsMbFyBz0mlXtQeed4q61nK6.jpeg', '2020-07-12 03:58:19', '2020-07-12 03:59:05');
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `product_ingredient`
+-- Estructura de tabla para la tabla `product_ingredients`
 --
 
-CREATE TABLE `product_ingredient` (
-  `id` int(255) NOT NULL,
-  `product_id` int(255) NOT NULL,
-  `ingredient_id` int(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+CREATE TABLE `product_ingredients` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `product_id` bigint(20) UNSIGNED NOT NULL,
+  `ingredient_id` bigint(20) UNSIGNED NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -70,21 +149,45 @@ CREATE TABLE `product_ingredient` (
 --
 
 CREATE TABLE `users` (
-  `id` int(255) NOT NULL,
-  `role` varchar(20) NOT NULL,
-  `name` varchar(100) NOT NULL,
-  `surname` varchar(200) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `birth_date` datetime NOT NULL,
-  `created_at` datetime NOT NULL,
-  `updated_at` datetime NOT NULL,
-  `remember_token` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `role` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `surname` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email_verified_at` timestamp NULL DEFAULT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `birth_date` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `users`
+--
+
+INSERT INTO `users` (`id`, `role`, `name`, `surname`, `email`, `email_verified_at`, `password`, `birth_date`, `remember_token`, `created_at`, `updated_at`) VALUES
+(4, '1', 'admin', 'admin', 'admin@gmail.com', NULL, '$2y$10$2hIoDx1BAeIUD99Vbjl2L.TKQNJloIo5aAwrzlZYygQznfbP0Grm6', '00/00/00', NULL, '2020-07-04 08:20:15', '2020-07-04 08:20:15'),
+(5, '1', 'Josué', 'Ruiz', 'jrc2899@gmail.com', NULL, '12345678', '28/02/1999', NULL, '2020-07-04 10:09:46', '2020-07-04 10:09:46'),
+(6, '1', 'Tarik', 'Diaz', 'diaztarik12@gmail.com', NULL, '$2y$10$UakYrntDBLYthF3jN0cFw.SGjvrQdzAbEX.5yKm.Q7mUt2MdmSQDS', '28/04/1985', NULL, '2020-07-04 10:14:37', '2020-07-04 10:14:37'),
+(7, '1', 'Geovanny', 'Fernandez', 'geovanex24@hotmail.com', NULL, '$2y$10$s0rgZvVqARDlAYW9qfOEi.bTrd.uSUtmdht39aBSxerWirt6hHx6y', '24/06/1999', NULL, '2020-07-04 10:18:45', '2020-07-04 10:18:45'),
+(8, '1', 'Fabricio', 'Vargas', 'Fabricio@homerito.com', NULL, '$2y$10$gIeEX0Fwf5atBduYIo3Gaevu55HMcntZAMv81jCFbKblSGnq.fHJy', '31/05/1999', NULL, '2020-07-04 10:19:31', '2020-07-04 10:19:31'),
+(9, '1', 'Joseph Mauricio', 'Morales', 'josephmms@gmail.com', NULL, '$2y$10$DfQZ7qkt/qkV/wr7tVagu.YCOMk1lR9ZuhhQQ/QCX4b6LasjmkryW', '27/11/1999', NULL, '2020-07-04 10:26:50', '2020-07-04 10:26:50'),
+(10, '2', 'Sebastian', 'Mesen', 'sebasmesen17@gmail.com', NULL, '$2y$10$mGHHtQy24hwLFYPhMInlz.lNQ.vWNmLTPBIZP2jNZNRj2jJITcygC', '17/12/1998', NULL, '2020-07-04 10:28:35', '2020-07-04 10:28:35'),
+(11, '2', 'Eidam', 'Montero', 'eidam2015@hotmail.com', NULL, '$2y$10$SLIUKdrScx.dOvLepkAznOerENlwf1IYaIVNQsMPqjE4rr0uqAIh.', '20/12/2015', NULL, '2020-07-04 10:30:14', '2020-07-04 10:30:14'),
+(12, '1', 'Josué', 'Ruiz', 'jrc2899@gmail5.com', NULL, '$2y$10$6qr2SllEyqs0tPdxB19EmeGAzfPW6B7oWw2TBkv41WHR67qfpXKFe', '1999-02-28', NULL, '2020-07-09 11:25:06', '2020-07-09 11:25:06'),
+(13, 'Admin', 'Joseph', 'Morales', 'josephmms1999@gmail.com', NULL, '$2y$10$YQTPPxfhvUY/mgHF9qUlXuY5C9W7uBeePZ.gpmay/2CLa4cL6joCG', '1999-09-27', NULL, '2020-07-12 03:47:33', '2020-07-12 03:47:33'),
+(14, 'Admin', 'Mauricio', 'Morales', 'mauricio@gmail.com', NULL, '$2y$10$fKiI0nwhPJc2w3c101lEwOQ7BAY/LjAHj0vhf7zUgarGIv60CPDeq', '1999-09-27', NULL, '2020-07-12 03:54:27', '2020-07-12 03:54:27');
 
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `failed_jobs`
+--
+ALTER TABLE `failed_jobs`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indices de la tabla `ingredients`
@@ -93,50 +196,88 @@ ALTER TABLE `ingredients`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indices de la tabla `product`
+-- Indices de la tabla `migrations`
 --
-ALTER TABLE `product`
+ALTER TABLE `migrations`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indices de la tabla `product_ingredient`
+-- Indices de la tabla `password_resets`
 --
-ALTER TABLE `product_ingredient`
-  ADD PRIMARY KEY (`id`,`product_id`,`ingredient_id`);
+ALTER TABLE `password_resets`
+  ADD KEY `password_resets_email_index` (`email`);
+
+--
+-- Indices de la tabla `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `product_ingredients`
+--
+ALTER TABLE `product_ingredients`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `product_ingredients_product_id_foreign` (`product_id`),
+  ADD KEY `product_ingredients_ingredient_id_foreign` (`ingredient_id`);
 
 --
 -- Indices de la tabla `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `users_email_unique` (`email`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
 --
+-- AUTO_INCREMENT de la tabla `failed_jobs`
+--
+ALTER TABLE `failed_jobs`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT de la tabla `ingredients`
 --
 ALTER TABLE `ingredients`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
--- AUTO_INCREMENT de la tabla `product`
+-- AUTO_INCREMENT de la tabla `migrations`
 --
-ALTER TABLE `product`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `migrations`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
--- AUTO_INCREMENT de la tabla `product_ingredient`
+-- AUTO_INCREMENT de la tabla `products`
 --
-ALTER TABLE `product_ingredient`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `products`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+
+--
+-- AUTO_INCREMENT de la tabla `product_ingredients`
+--
+ALTER TABLE `product_ingredients`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `product_ingredients`
+--
+ALTER TABLE `product_ingredients`
+  ADD CONSTRAINT `product_ingredients_ingredient_id_foreign` FOREIGN KEY (`ingredient_id`) REFERENCES `ingredients` (`id`),
+  ADD CONSTRAINT `product_ingredients_product_id_foreign` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
