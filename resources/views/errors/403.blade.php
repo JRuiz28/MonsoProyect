e5a1e1f54d12a85b29c2bd52e99f571fc70e77e5
Josue <josue7114@gmail.com>
2020-07-10 23:00:35 -0600

    Personalización[C páginas 404 y 403

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -1,4 +1,26 @@
-@extends('errors::layout')
-
-@section('title', '403 - Acceso no autorizado')
-@section('message', 'Acceso no autorizado')
+@extends('HeadFoot')
+@section('header')
+    @parent
+@endsection
+@section('content')
+    <body>
+        <section class="section">
+            <div class="section__container-img">
+                <img class="section__container-img--403" src="/images/403.png"/>
+            </div>
+            <div class="section__container-paragraph">
+                <p class="section__container-paragraph--paragraph">No tienes permitido acceder acá.
+                </p>
+                <button class="section__container-paragraph--button">
+                    Volver a inicio
+                </button>
+            </div>
+            <div>
+                
+            </div>
+        </section>
+    </body>
+@endsection
+@section('footer')
+    @parent
+@endsection
--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,4 +1,26 @@
-@extends('errors::layout')
-
-@section('title', '404 - Página no encontrada')
-@section('message', 'No se ha encontrado la página que buscas')
+@extends('HeadFoot')
+@section('header')
+    @parent
+@endsection
+@section('content')
+    <body>
+        <section class="section">
+            <div class="section__container-img">
+                <img class="section__container-img--404" src="/images/404.png"/>
+            </div>
+            <div class="section__container-paragraph">
+                <p class="section__container-paragraph--paragraph">La página que estabas buscando, no se encuentra disponible.
+                </p>
+                <button class="section__container-paragraph--button">
+                    Volver a inicio
+                </button>
+            </div>
+            <div>
+                
+            </div>
+        </section>
+    </body>
+@endsection
+@section('footer')
+    @parent
+@endsection
